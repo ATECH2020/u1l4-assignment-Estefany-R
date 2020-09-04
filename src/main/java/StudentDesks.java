172868553dc1944d9087f5760ce97510d01363ae
Estefany Rodriguez <estefany.233763@nv.ccsd.net>
2020-09-04 22:22:58 +0000

    Completed StudentsDesk

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,12 +7,21 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        
+        int class1 = scanner.nextInt();
+        int class2 = scanner.nextInt();
+        int class3 = scanner.nextInt();
 
         /*
          *  your code goes here
          */
+         double totalDesk = class1 + class2 + class3;
+         double SumTotal = totalDesk/2;
 
+         System.out.print(Math.round(SumTotal));
+
+
+         
+         
 
         // closing the scanner object
         scanner.close();
